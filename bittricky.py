a6bc14d3b1484c6e4c15500f39ab107d9f8915ae
MadhumithaAmirishetty <amirishettymadhumitha14@gmail.com>
2026-04-30 14:58:13 +0530

    Create bittricky.py

---
+++ bittricky.py
@@ -0,0 +1,14 @@
+name=["SanD","hRu","SARa"]
+marks=[[40,85,40],[40,60,50],[20,30,50]]
+pos=1
+for i in range(3):
+    avg=(sum(marks[i])//len(marks[i]))
+    if(avg>80):
+        print("{}.Ms {} has scored {}.% - S Grade".format(pos,name[i].title(),avg))
+    elif(avg>40 and avg<60):
+        print("{}.Ms {} has scored {}.% - B Grade".format(pos,name[i].title(),avg))
+    elif(avg>60 and avg>80):
+        print("{}.Ms {} has scored {}.% - A Grade".format(pos,name[i].title(),avg))
+    else:
+          print("{}.Ms {} has scored {}.% - Failed".format(pos,name[i].title(),avg))
+    pos=pos+1
